653d0d9371f8beb66cfc8699dda9cdc7efa0b210
alec <alecandersen97@gmail.com>
2025-02-27 12:06:16 -0500

    Can add rows to Games table

--- db_microservice.py
+++ db_microservice.py
@@ -5,6 +5,17 @@ import zmq
 DATABASE_FILE = "monty_hall.db"
 SCHEMA_FILE = "schema.sql"
 
+
+def add_game(cursor, gameID, playerID, result):
+    game = f"""
+    INSERT INTO Games VALUES(
+    {gameID},
+    {playerID},
+    {result}
+    );
+    """
+    cursor.execute(game)
+
 def main():
     connection = sqlite3.connect(DATABASE_FILE)
     cursor = connection.cursor()
