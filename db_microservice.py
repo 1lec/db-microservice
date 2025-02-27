9fcf6bdb7b0db7cda2e22ed351fade2e2ce3d0f0
alec <alecandersen97@gmail.com>
2025-02-27 11:47:02 -0500

    Service imports schema and creates db with tables

--- db_microservice.py
+++ db_microservice.py
@@ -2,8 +2,19 @@ import sqlite3
 import zmq
 
 
+DATABASE_FILE = "monty_hall.db"
+SCHEMA_FILE = "schema.sql"
+
 def main():
-    pass
+    connection = sqlite3.connect(DATABASE_FILE)
+    cursor = connection.cursor()
+
+    with open(SCHEMA_FILE) as schema_file:
+        schema = schema_file.read()
+        cursor.executescript(schema)
+
+    connection.commit()
+    connection.close()
 
 
 if __name__ == "__main__":
---
+++ schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS Games (
+    gameID      INT,
+    playerID    INT,
+    result      INT
+);
