f924c1fc8ad41b1774438485982e394c23f19962
alec <alecandersen97@gmail.com>
2025-02-27 12:55:32 -0500

    Create DatabaseManager class and refactor

--- db_microservice.py
+++ db_microservice.py
@@ -4,28 +4,38 @@ import zmq
 
 DATABASE_FILE = "monty_hall.db"
 SCHEMA_FILE = "schema.sql"
+ZMQ_PORT = "5557"
 
 
-def add_game(cursor, gameID, playerID, result):
-    game = f"""
-    INSERT INTO Games VALUES(
-    {gameID},
-    {playerID},
-    {result}
-    );
-    """
-    cursor.execute(game)
+class DatabaseManager:
+    """Handles all client requests and interacts with the database."""
+    def __init__(self):
+        self.connection = sqlite3.connect(DATABASE_FILE)
+        self.cursor = self.connection.cursor()
+
+    def add_game(self, gameID, playerID, result):
+        game = f"""
+        INSERT INTO Games VALUES(
+        {gameID},
+        {playerID},
+        {result}
+        );
+        """
+        self.cursor.execute(game)
+        self.connection.commit()
+
+    def listen(self):
+        with open(SCHEMA_FILE) as schema_file:
+            schema = schema_file.read()
+            self.cursor.executescript(schema)
+
+        self.connection.commit()
+        self.connection.close()
+
 
 def main():
-    connection = sqlite3.connect(DATABASE_FILE)
-    cursor = connection.cursor()
-
-    with open(SCHEMA_FILE) as schema_file:
-        schema = schema_file.read()
-        cursor.executescript(schema)
-
-    connection.commit()
-    connection.close()
+    db = DatabaseManager()
+    db.listen()
 
 
 if __name__ == "__main__":
