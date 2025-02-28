ba62e889092ca086f530657e7f929a6e16d80ca9
alec <alecandersen97@gmail.com>
2025-02-28 15:12:08 -0500

    Create functions in test program

--- test_program.py
+++ test_program.py
@@ -1,22 +1,31 @@
 import zmq
 
-
-def main():
-    context = zmq.Context()
-    socket = context.socket(zmq.REQ)
-    socket.connect("tcp://localhost:5557")
-
-    # Add a game
-    socket.send_json({"type": "game", "name": "Eden", "result": 1})
+def add_game(socket, name, result):
+    socket.send_json({"type": "game", "name": name, "result": result})
     print(socket.recv().decode())
 
-    # Get all results for a player
-    socket.send_json({"type": "player", "name": "Eden"})
+def delete_player(socket, name):
+    socket.send_json({"type": "delete", "name": name})
+    print(socket.recv().decode())
+
+def delete_all_players(socket):
+    socket.send_json({"type": "delete-all"})
+    print(socket.recv().decode())
+
+def get_games(socket, name):
+    socket.send_json({"type": "player", "name": name})
     response = socket.recv_json()
     if response["status"] == "success":
         print(response["games"])
     else:
         print(response["message"])
+
+
+def main():
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5557")
+    
 
 if __name__ == "__main__":
     main()
